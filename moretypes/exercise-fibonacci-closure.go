704ea4b60c2095b344e36de0e9dce7a75fca974d
Mark Smith <smith.h.mark@gmail.com>
2018-02-16 13:11:12 -0800

    added fib closure

--- moretypes/exercise-fibonacci-closure.go
+++ moretypes/exercise-fibonacci-closure.go
@@ -5,6 +5,14 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+        a := -1
+        b := 1
+        return func () int {
+                c := a + b
+                a = b
+                b = c
+                return c
+        }
 }
 
 func main() {
